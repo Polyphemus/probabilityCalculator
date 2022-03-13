4bd5680b84b913ccaa4beec3d32d0223c4212ab2
Polyphemus <beans4258@gmail.com>
2022-03-13 18:52:23 -0400

    Fixed experiment function

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,7 +1,5 @@
 import copy
 import random
-# Consider using the modules imported above.
-
 
 class Hat:
     def __init__(self, **kwargs):
@@ -15,9 +13,9 @@ class Hat:
         if number >= len(self.contents):
             return self.contents
         for i in range(number):
-            x = random.randrange(len(self.contents))
-            draws.append(self.contents[x])
-            self.contents.pop(x)
+            x = random.randrange(len(self.contents)) # x is a random int between 0 and len of contents
+            draws.append(self.contents[x]) # add contents[x] to draws
+            self.contents.pop(x) # remote contents[x] from contents
         return draws
 
 
@@ -26,8 +24,7 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     for i in range(num_experiments):
         expHat = copy.deepcopy(hat)
         expDraw = expHat.draw(num_balls_drawn)
-        if all(v >= expDraw.count(k) for k, v in expected_balls.items()):
+        # if the count of every color in the experimental draw  is greater than the corresponding dictionary value, success
+        if all(expDraw.count(k) >= v for k, v in expected_balls.items()):
             successes += 1
     return successes/num_experiments
-
-## use small numbers of experiments, print out results
